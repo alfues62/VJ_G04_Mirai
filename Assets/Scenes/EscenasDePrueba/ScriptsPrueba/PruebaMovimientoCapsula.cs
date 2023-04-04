dfc2f9612cbbad56b7821298b60632e73eae29d7
Xtar <Alberto2alonso2001@gmail.com>
2023-04-04 11:46:47 +0200

    Revert "Carcel y edificio interior junto con shuriken"
    This reverts commit 1776ea2fd30ca8f6ccb91b24b4530d4494c96843.

--- Assets/Scenes/EscenasDePrueba/ScriptsPrueba/PruebaMovimientoCapsula.cs
+++ Assets/Scenes/EscenasDePrueba/ScriptsPrueba/PruebaMovimientoCapsula.cs
@@ -173,39 +173,38 @@ public class PruebaMovimientoCapsula : MonoBehaviour
 
         // Mode - Air
         else
-
+<<<<<<< HEAD
+        {
+            state = MovementState.air;
+=======
         {
             state = MovementState.air;
 
-            {
-                state = MovementState.air;
-
-                if (desiredMoveSpeed < sprintSpeed)
-                    desiredMoveSpeed = walkSpeed;
-                else
-                    desiredMoveSpeed = sprintSpeed;
-            }
-
-            bool desiredMoveSpeedHasChanged = desiredMoveSpeed != lastDesiredMoveSpeed;
-            if (lastState == MovementState.dashing) keepMomentum = true;
-
-            if (desiredMoveSpeedHasChanged)
-            {
-                if (keepMomentum)
-                {
-                    StopAllCoroutines();
-                    StartCoroutine(SmoothlyLerpMoveSpeed());
-                }
-                else
-                {
-                    StopAllCoroutines();
-                    moveSpeed = desiredMoveSpeed;
-                }
-            }
-
-            lastDesiredMoveSpeed = desiredMoveSpeed;
-            lastState = state;
+            if (desiredMoveSpeed < sprintSpeed)
+                desiredMoveSpeed = walkSpeed;
+            else
+                desiredMoveSpeed = sprintSpeed;
         }
+
+        bool desiredMoveSpeedHasChanged = desiredMoveSpeed != lastDesiredMoveSpeed;
+        if (lastState == MovementState.dashing) keepMomentum = true;
+
+        if (desiredMoveSpeedHasChanged)
+        {
+            if (keepMomentum)
+            {
+                StopAllCoroutines();
+                StartCoroutine(SmoothlyLerpMoveSpeed());
+            }
+            else
+            {
+                StopAllCoroutines();
+                moveSpeed = desiredMoveSpeed;
+            }
+        }
+
+        lastDesiredMoveSpeed = desiredMoveSpeed;
+        lastState = state;
     }
 
     private float speedChangeFactor;
@@ -225,7 +224,7 @@ public class PruebaMovimientoCapsula : MonoBehaviour
             time += Time.deltaTime * boostFactor;
 
             yield return null;
-
+>>>>>>> alexander
         }
 
         moveSpeed = desiredMoveSpeed;
