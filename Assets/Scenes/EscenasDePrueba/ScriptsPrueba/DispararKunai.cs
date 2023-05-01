8b6c4f920797593ae4ea61e2067be4efe09256f0
Xtar <Alberto2alonso2001@gmail.com>
2023-05-01 17:44:46 +0200

    Cambio Municion

--- Assets/Scenes/EscenasDePrueba/ScriptsPrueba/DispararKunai.cs
+++ Assets/Scenes/EscenasDePrueba/ScriptsPrueba/DispararKunai.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class DispararKunai : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject bullet;
+    public Transform spawnpoint;
+
+    public float shotForce = 1500;
+    public float shotRate = 0.5f;
+
+    private float shotRateTime;
+    private int balasRestantes = 10;
+    
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (Time.time > shotRateTime && balasRestantes > 0)
+            {
+                GameObject newBullet;
+                newBullet = Instantiate(bullet, spawnpoint.position, spawnpoint.rotation);
+                newBullet.GetComponent<Rigidbody>().AddForce(spawnpoint.forward * shotForce);
+                shotRateTime = Time.time + shotRate;
+                Destroy(newBullet, 2);
+                balasRestantes--;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            VidaEnemigos vidaEnemigo = collision.gameObject.GetComponent<VidaEnemigos>();
+            if (vidaEnemigo != null)
+            {
+                vidaEnemigo.vida -= 10;
+            }
+            Destroy(gameObject);
+        }
     }
 }
