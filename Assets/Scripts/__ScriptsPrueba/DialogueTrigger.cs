4e3eea7dd73aa9e4d823b1a99e09b9cb527a7a28
alfues62 <94858416+alfues62@users.noreply.github.com>
2023-05-02 03:52:39 +0200

    Enemigos Añadidos A Escena
    aa

--- Assets/Scripts/__ScriptsPrueba/DialogueTrigger.cs
+++ Assets/Scripts/__ScriptsPrueba/DialogueTrigger.cs
@@ -6,32 +6,33 @@ public class DialogueTrigger : MonoBehaviour
 {
 
     public Dialogue dialogue;
- 
+    public bool autoStart = false;
     public float interactionRange = 2f;
     public bool isInRange = false;
     public float rangoDeAlerta;
-
     public LayerMask capaDelJugador;
-
-
-    private void Start()
-    {
-
-    }
-
-
- 
 
     private void Update()
     {
-
         isInRange = Physics.CheckSphere(transform.position, interactionRange, capaDelJugador);
 
-            if (isInRange && Input.GetKeyDown(KeyCode.F))
+        if (isInRange)
         {
-       
+            if (autoStart)
+            {
+                TriggerDialogue();
+                AutoStartoff();
+            }
+            else if (Input.GetKeyDown(KeyCode.F))
                 TriggerDialogue();
         }
+
+
+    }
+
+    public void AutoStartoff()
+    {
+        autoStart = false;
     }
 
     public void TriggerDialogue()
